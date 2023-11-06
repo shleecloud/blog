546eab6e53ff36cb9245efffd0ddc60615a6e50a
shleecloud <shleecloud@gmail.com>
2023-11-06 23:55:03 +0900

    edit: layout design

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -8,7 +8,9 @@ export default function Comments({ slug }: { slug: string }) {
   const [loadComments, setLoadComments] = useState(false)
   return (
     <>
-      {!loadComments && <button onClick={() => setLoadComments(true)}>Load Comments</button>}
+      {!loadComments && (
+        <button onClick={() => setLoadComments(true)}>Click to Load Comments</button>
+      )}
       {siteMetadata.comments && loadComments && (
         <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
       )}
