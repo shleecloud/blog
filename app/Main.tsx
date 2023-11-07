8d36bcc2e4e74fadca4445eed49b507ab7bc41a1
shleecloud <shleecloud@gmail.com>
2023-11-07 11:33:01 +0900

    edit: favicon and logo

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,7 +3,6 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 // import NewsletterForm from 'pliny/ui/NewsletterForm'
-import Hero from '@/components/Hero'
 
 const MAX_DISPLAY = 5
 
@@ -12,13 +11,13 @@ export default function Home({ posts }) {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <Hero />
+          {/*<Hero />*/}
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
+            안녕하세요!
           </h1>
-          {/*<p className="text-lg leading-7 text-gray-500 dark:text-gray-400">*/}
-          {/*  {siteMetadata.description}*/}
-          {/*</p>*/}
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            {siteMetadata.description}
+          </p>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,16 +5,23 @@ import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
-
+import Image from '@/components/Image'
 const Header = () => {
   return (
     <header className="flex items-center justify-between py-10">
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
-            {/*<div className="mr-3">*/}
-            {/*  <Logo />*/}
-            {/*</div>*/}
+            <div className="mr-3">
+              {/*<Logo />*/}
+              <Image
+                src={'/static/images/avatar.png'}
+                alt={'logo'}
+                width={40}
+                height={40}
+                className={'rounded-full'}
+              />
+            </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
                 {siteMetadata.headerTitle}
