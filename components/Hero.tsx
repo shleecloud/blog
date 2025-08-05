1f36c3f6d3017c7261f7993d6174531a75e4e281
shleecloud <shleecloud@gmail.com>
2025-08-06 00:25:25 +0900

    fix: prettier 포맷팅으로 tailwind 순서 변경

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ export default function Hero() {
   return (
     <div className="my-6 flex flex-col justify-center gap-x-12 xl:mb-12 xl:flex-row">
       <div className="max-w-2xl pt-6">
-        <h1 className="pb-6 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+        <h1 className="pb-6 text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
           Hi, I’m shleecloud
         </h1>
         <h2 className="prose text-lg text-gray-600 dark:text-gray-400">
